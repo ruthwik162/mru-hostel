722b212b1860e52595dd5b206eb9c5d7767ae1e5
M.Nagaruthwik <nagaruthwikmerugu162@gmail.com>
2025-06-04 18:21:48 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/malla-reddy-university/',
   plugins: [react(),
     tailwindcss(),
   ],
